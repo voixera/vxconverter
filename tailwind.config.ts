a7ee6a954d7fa3f357be9854b2cd42e9d1913453
drx <anonymous666drx@gmail.com>
2026-09-18 17:54:00 +0700

    fix: explicit nextjs framework in vercel.json and remove vercelignore to fix VCR container error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   darkMode: ["class"],
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./web/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./web/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
